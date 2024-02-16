9d16f3e7e94e4784b97c43293e9fd4ad3fac460e
idowudamolaolatunji <olatunjiidowuoyin06@gmail.com>
2024-02-16 18:49:16 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,19 @@
+'use strict';
+
+const btn = document.querySelector('#refresh-button');
+
+async function fetchDog() {
+    const res = await fetch('https://dog.ceo/api/breeds/image/random');
+    const data = await res.json();
+    return data;
+    // console.log(data);
+}
+
+
+btn.addEventListener('click', async function() {
+    const dogImage = await fetchDog();
+    console.log(dogImage.message)
+
+    document.querySelector('#dog-image').src = dogImage.message
+
+})
